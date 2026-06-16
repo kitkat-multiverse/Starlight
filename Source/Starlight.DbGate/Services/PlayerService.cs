8826a90f0dec97cbf9cdd502f2262d96fb3728bb
KingRainbow44 <kobedo11@gmail.com>
2026-06-16 18:36:31 -0400

    fix(db): use `ServiceScopedFactory` for db access

--- Source/Starlight.DbGate/Services/PlayerService.cs
+++ Source/Starlight.DbGate/Services/PlayerService.cs
@@ -1,12 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
 using Starlight.Rpc;
 using Starlight.Rpc.Proto;
 
 namespace Starlight.DbGate.Services;
 
-public sealed class PlayerService(StarlightDbContext db)
+public sealed class PlayerService(IServiceScopeFactory scopes)
 {
     public async Task Fetch(FetchPlayerReq msg, RpcMessage rpc)
     {
+        using var scope = scopes.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<StarlightDbContext>();
+
         var player = await db.Players.FindAsync(msg.Uid);
 
         await rpc.Reply(new FetchPlayerRsp {
