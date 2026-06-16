4e5df4bad8175d6d4e7030002a73bdd5a47a0ec2
Hiro <84511935+Hiro420@users.noreply.github.com>
2026-06-16 20:03:51 +0300

    Update Quests.cs

--- Libraries/Starlight.Game.Resources/Excel/Quests.cs
+++ Libraries/Starlight.Game.Resources/Excel/Quests.cs
@@ -9,5 +9,5 @@ public sealed class CoopPointData : Data {
     [JsonPropertyName("acceptQuest")]
     public uint AcceptQuest { get; set; }
     [JsonPropertyName("postPointList")]
-    public uint[] PostPointList { get; set; } = new uint[0];
+    public uint[] PostPointList { get; set; } = [];
 }
