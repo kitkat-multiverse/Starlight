e8d4843ebeb519a845294daa2f14896478703cba
KingRainbow44 <kobedo11@gmail.com>
2026-06-16 16:13:21 -0400

    init(rpc): add RPC framework
    should hopefully unblock a lot of things for us

--- Libraries/Starlight.Common/Extensions.cs
+++ Libraries/Starlight.Common/Extensions.cs
@@ -1,5 +1,21 @@
 namespace Starlight.Common;
 
+public static class RandomExtensions
+{
+    /// <summary>
+    /// Generates a random version 4 UUID.
+    /// </summary>
+    public static Guid NextUuid(this Random random) {
+        Span<byte> bytes = stackalloc byte[16];
+        random.NextBytes(bytes);
+
+        bytes[6] = (byte)(bytes[6] & 0x0F | 0x40); // version 4
+        bytes[8] = (byte)(bytes[8] & 0x3F | 0x80); // variant 1 (RFC 4122)
+
+        return new Guid(bytes);
+    }
+}
+
 public static class StringExtensions
 {
     /// <summary>
