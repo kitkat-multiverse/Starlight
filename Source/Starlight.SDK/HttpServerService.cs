10ef6eff6ccd1cb9c8ef6a62f9537a3689933a55
KingRainbow44 <kobedo11@gmail.com>
2026-06-16 14:39:06 -0400

    misc(sdk): move `AddSdkServer` to `HttpServerService.cs`
    it's like the `Program.cs` of a service since they can't run independently (although we can probably make them run independently later)

--- Source/Starlight.SDK/HttpServerService.cs
+++ Source/Starlight.SDK/HttpServerService.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Starlight.Database.DependencyInjection;
+using Starlight.SDK.Database;
+using Starlight.SDK.Database.Impl;
 
 namespace Starlight.SDK;
 
@@ -13,11 +16,11 @@ public sealed class HttpServerService(StarlightConfig config) : BackgroundServic
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var builder = WebApplication.CreateBuilder();
-        
+
         builder.Services
             .AddSerilog()
             .AddSingleton(_ => config);
-        
+
         // NOTE: If you wish to use SSL, do so behind a reverse proxy.
         builder.WebHost.UseUrls($"http://{config.Server.Http.BindAddress}:{config.Server.Http.BindPort}");
 
@@ -26,5 +29,24 @@ public sealed class HttpServerService(StarlightConfig config) : BackgroundServic
         app.MapGet("/", () => Results.Ok("Starlight"));
 
         await app.RunAsync(stoppingToken);
+    }
+}
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddSdkServer(this IServiceCollection collection, StarlightConfig config)
+    {
+        switch (DatabaseHelper.ParseProvider(config.Database.ConnectionString, out var connString))
+        {
+            case ProviderType.Sqlite: {
+                collection.AddStarlightDatabase(connString, config.Database.Sqlite, typeof(HttpServerService).Assembly);
+                collection.AddSingleton<IAccountRepository, SqliteAccountRepository>();
+                break;
+            }
+        }
+
+        collection.AddHostedService<HttpServerService>();
+
+        return collection;
     }
 }
